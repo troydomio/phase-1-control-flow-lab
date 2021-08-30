7d7362a680224a733d6d8ae5ff42949a8220daf9
Troy <troyakins99@yahoo.com>
2021-08-30 10:01:04 -0500

    commit tree 797c81ad46b7ab81829db02ff8e3e4efd441574e

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let rideLength;
+  if(feet <= 400){rideLength = 'This one is on me!';}
+   else if(feet >= 2000 && feet < 2500){rideLength = 'I will gladly take your thirty bucks.'} 
+   else if (feet > 2500) rideLength = 'No can do.'
+   return rideLength;
+}
+  
+ 
+
+function ternaryCheckCity(city){
+  let vacation =  (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  return vacation;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ let tipMessage;
+ if (tip === 'generous'){tipMessage= 'Thank you so much.'}
+ else if (tip === 'not as generous'){tipMessage = 'Thank you.'}
+ else (tipMessage = "Bye.")
+ return tipMessage;
 }
